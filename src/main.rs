285ea06d406cf7c2756307fb491628839f550e47
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-31 10:15:42 +0300

    Add basic command handling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::{self, Write};
 
 fn main() {
+    let existing_commands = vec!["cd", "exit", "help", "ls", "pwd"];
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -9,4 +10,14 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    for cmd in existing_commands {
+        if input.trim() == cmd {
+            println!("{} command found", cmd);
+            return;
+        } else {
+            print!("{}: command not found", input.trim());
+            return;
+        }
+    }
 }
