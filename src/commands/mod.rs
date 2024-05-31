16102ba940fe67eed301e2a25fd32a77c2e9ee5e
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-31 18:54:26 +0300

    Add pwd command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,15 +1,16 @@
-use std::collections::HashMap;
+use std::{ collections::HashMap, env };
 use crate::helpers::path;
 
 type ICommand = dyn Fn(Vec<String>);
 
-const SUPPORTED_COMMANDS: [&str; 3] = ["echo", "type", "exit"];
+const SUPPORTED_COMMANDS: [&str; 4] = ["echo", "type", "exit", "pwd"];
 
 pub fn get_commands() -> HashMap<&'static str, &'static ICommand> {
     let mut map: HashMap<&str, &ICommand> = HashMap::new();
     map.insert("type", &type_);
     map.insert("echo", &echo);
     map.insert("exit", &exit);
+    map.insert("pwd", &pwd);
     map
 }
 
@@ -36,5 +37,12 @@ pub fn exit(inputs: Vec<String>) {
         std::process::exit(0);
     } else {
         print!("{}: command not found", inputs[0]);
+    }
+}
+
+pub fn pwd(_inputs: Vec<String>) {
+    match env::current_dir() {
+        Ok(path) => print!("{}", path.to_str().unwrap()),
+        Err(_) => print!("failed to get current directory"),
     }
 }
