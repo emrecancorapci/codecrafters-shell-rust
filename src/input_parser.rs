30f3cc9fa4e4bd4e08aa8b0a48f1150b853ee57f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2025-01-18 01:15:20 +0300

    Add input parser

---
+++ src/input_parser.rs
@@ -0,0 +1,146 @@
+#[allow(unused_assignments)]
+use std::io::{Error, ErrorKind};
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Input {
+    Command(String),
+    Argument(String, bool),
+    String(String, bool),
+}
+
+#[derive(PartialEq, Eq)]
+enum ParseMode {
+    None,
+    Value,
+    StringSingle,
+    StringDouble,
+    SingleDashArg,
+    DoubleDashArg,
+}
+
+pub struct InputParser {
+    temp: String,
+    mode: ParseMode,
+    result: Vec<Input>,
+}
+
+impl InputParser {
+    pub fn new() -> InputParser {
+        InputParser {
+            temp: String::new(),
+            mode: ParseMode::None,
+            result: Vec::new(),
+        }
+    }
+
+    pub fn parse(&mut self, str: &String) -> Result<&Vec<Input>, Error> {
+        let mut iter = str.chars().into_iter().enumerate().peekable();
+
+        while let Some((i, ch)) = iter.next() {
+            match self.mode {
+                ParseMode::None => match ch {
+                    '\'' => self.mode = ParseMode::StringSingle,
+                    '"' => self.mode = ParseMode::StringDouble,
+                    '-' => {
+                        if iter.peek() == Some(&(i + 1, '-')) {
+                            iter.next();
+                            self.mode = ParseMode::DoubleDashArg
+                        } else {
+                            self.mode = ParseMode::SingleDashArg
+                        }
+                    }
+                    'a'..='z' | 'A'..='Z' | '0'..='9' | '_' if self.temp.is_empty() => {
+                        self.mode = ParseMode::Value;
+                        self.temp.push(ch);
+                    }
+                    ' ' => {}
+                    _ => {
+                        return Err(Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("Invalid character at {}", i),
+                        ))
+                    }
+                },
+                ParseMode::Value => match ch {
+                    'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' => self.temp.push(ch),
+                    ' ' => self.push_input(),
+                    _ => {
+                        return Err(Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("Invalid character at {}", i),
+                        ))
+                    }
+                },
+                ParseMode::StringSingle => match ch {
+                    '\'' => self.push_input(),
+                    _ => self.temp.push(ch),
+                },
+                ParseMode::StringDouble => match ch {
+                    '"' => self.push_input(),
+                    _ => self.temp.push(ch),
+                },
+                ParseMode::SingleDashArg => match ch {
+                    'a'..='z' | 'A'..='Z' | '_' | '-' => self.temp.push(ch),
+                    ' ' => self.push_input(),
+                    _ => {
+                        return Err(Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("Invalid character at {}", i),
+                        ))
+                    }
+                },
+                ParseMode::DoubleDashArg => match ch {
+                    'a'..='z' | 'A'..='Z' | '_' | '-' => self.temp.push(ch),
+                    ' ' => self.push_input(),
+                    _ => {
+                        return Err(Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("Invalid character at {}", i),
+                        ))
+                    }
+                },
+            }
+        }
+
+        match self.mode {
+            ParseMode::StringSingle => {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    "Single quote didn't end.",
+                ))
+            }
+            ParseMode::StringDouble => {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    "Double quote didn't end.",
+                ))
+            }
+            ParseMode::None => {
+                return Ok(&self.result);
+            }
+            _ => {
+                self.push_input();
+                return Ok(&self.result);
+            }
+        }
+    }
+
+    fn push_input(&mut self) {
+        match self.mode {
+            ParseMode::None => panic!("This shouldn't have happened!"),
+            ParseMode::Value => self.result.push(Input::Command(self.temp.to_string())),
+            ParseMode::StringSingle => self
+                .result
+                .push(Input::String(self.temp.to_string(), false)),
+            ParseMode::StringDouble => self.result.push(Input::String(self.temp.to_string(), true)),
+            ParseMode::SingleDashArg => self
+                .result
+                .push(Input::Argument(self.temp.to_string(), false)),
+            ParseMode::DoubleDashArg => self
+                .result
+                .push(Input::Argument(self.temp.to_string(), true)),
+        }
+        self.temp = String::new();
+        self.mode = ParseMode::None;
+    }
+}
---
+++ tests/input_parser.rs
@@ -0,0 +1,201 @@
+use shell_starter_rust::input_parser::{Input, InputParser};
+
+#[test]
+fn hello_world() {
+    let test_string = "hello world".to_string();
+    let expected_result = vec![
+        Input::Command("hello".to_string()),
+        Input::Command("world".to_string()),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn spacey_hello_world() {
+    let test_string = "hello                 world".to_string();
+    let expected_result = vec![
+        Input::Command("hello".to_string()),
+        Input::Command("world".to_string()),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn echo_hello_world_single_quote() {
+    let test_string = "echo 'hello world'".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::String("hello world".to_string(), false),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn echo_hello_world_double_quote() {
+    let test_string = "echo \"hello world\"".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::String("hello world".to_string(), true),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn spacey_echo_hello_world() {
+    let test_string = "echo \"hello                   world\"".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::String("hello                   world".to_string(), true),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn double_inside_single_quote() {
+    let test_string = "echo '\"hello world\"'".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::String("\"hello world\"".to_string(), false),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn single_inside_double_quote() {
+    let test_string = "echo \"'hello world'\"".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::String("'hello world'".to_string(), true),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn single_dash_argument() {
+    let test_string = "echo -s 'hello world'".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::Argument("s".to_string(), false),
+        Input::String("hello world".to_string(), false),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn double_dash_argument() {
+    let test_string = "echo --silent 'hello world'".to_string();
+    let expected_result = vec![
+        Input::Command("echo".to_string()),
+        Input::Argument("silent".to_string(), true),
+        Input::String("hello world".to_string(), false),
+    ];
+
+    let mut parser = InputParser::new();
+
+    match parser.parse(&test_string) {
+        Ok(parsed_vector) => {
+            assert_vec_eq(parsed_vector, &expected_result);
+        }
+        Err(err) => {
+            eprintln!("ERR: {}", err);
+            assert!(false);
+        }
+    }
+}
+
+fn assert_vec_eq<T: std::fmt::Debug + PartialEq>(vec1: &[T], vec2: &[T]) {
+    if vec1 != vec2 {
+        panic!(
+            "Vectors are not equal.\nLeft: {:?}\nRight: {:?}",
+            vec1, vec2
+        );
+    }
+}
