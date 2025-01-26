4f85b6c8bf43003e596a40f567bc83ccee2bf187
Emre Can Çorapçı <emrecancorapci@gmail.com>
2025-01-26 22:49:07 +0300

    Update tokenizer to turn it into a function, Remove parser.rs

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -8,15 +8,8 @@ pub use token::Token;
 
 mod token;
 
-pub struct Tokenizer {
-    temp: String,
-    mode: ParseMode,
-    sub_mode: ParseMode,
-    tokens: Vec<Token>,
-}
-
 #[derive(PartialEq, Eq)]
-pub(super) enum ParseMode {
+enum ParseMode {
     None,
     Value,
     SingleQuote,
@@ -25,217 +18,206 @@ pub(super) enum ParseMode {
     DoubleDashArg,
 }
 
-impl Tokenizer {
-    pub fn tokenize(input: String) -> Result<Vec<Token>, Error> {
-        let mut iter = input.chars().into_iter().enumerate().peekable();
-        let mut tokenizer = Tokenizer {
-            temp: String::new(),
-            mode: ParseMode::None,
-            sub_mode: ParseMode::None,
-            tokens: Vec::new(),
-        };
+pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
+    let mut iter = input.chars().into_iter().enumerate().peekable();
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut buffer = String::new();
+    let mut mode = ParseMode::None;
+    let mut sub_mode = ParseMode::None;
 
-        while let Some((i, ch)) = iter.next() {
-            match tokenizer.mode {
-                ParseMode::None => match ch {
-                    '\'' => tokenizer.mode = ParseMode::SingleQuote,
-                    '"' => tokenizer.mode = ParseMode::DoubleQuote,
-                    '\\' => {
-                        tokenizer.mode = ParseMode::Value;
-                        let ch = iter.peek();
+    while let Some((i, ch)) = iter.next() {
+        match mode {
+            ParseMode::None => match ch {
+                '\'' => mode = ParseMode::SingleQuote,
+                '"' => mode = ParseMode::DoubleQuote,
+                '\\' => {
+                    mode = ParseMode::Value;
 
-                        match ch {
-                            Some(_) => {
-                                let (_index, ch) = iter.next().unwrap();
+                    match iter.peek() {
+                        Some(_) => {
+                            let (_index, ch) = iter.next().unwrap();
 
-                                tokenizer.temp.push(ch)
-                            }
-                            None => todo!(),
+                            buffer.push(ch)
                         }
+                        None => todo!(),
                     }
-                    '-' => {
-                        if iter.peek() == Some(&(i + 1, '-')) {
-                            iter.next();
-                            tokenizer.mode = ParseMode::DoubleDashArg
-                        } else {
-                            tokenizer.mode = ParseMode::SingleDashArg
+                }
+                '-' => {
+                    if matches!(iter.peek(), Some(&(_, '-'))) {
+                        iter.next();
+                        mode = ParseMode::DoubleDashArg
+                    } else {
+                        mode = ParseMode::SingleDashArg
+                    }
+                }
+                'a'..='z' | 'A'..='Z' | '_' | '.' | '/' | '~' if buffer.is_empty() => {
+                    mode = ParseMode::Value;
+                    buffer.push(ch);
+                }
+                '0'..='9' if buffer.is_empty() => {
+                    if let Some((_, '>')) = iter.peek() {
+                        iter.next();
+                        tokens.push(parse_redirector(&mut iter, ch)?)
+                    } else {
+                        buffer.push(ch);
+                        mode = ParseMode::Value;
+                    }
+                }
+                '>' => tokens.push(parse_redirector(&mut iter, '1')?),
+                ' ' => {
+                    if tokens.last() != Some(&Token::Space) {
+                        tokens.push(Token::Space)
+                    }
+                }
+                _ => {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        format!("Invalid character at {}", i),
+                    ))
+                }
+            },
+            ParseMode::Value => match ch {
+                'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '.' | '/' => buffer.push(ch),
+                '\\' => {
+                    let ch = iter.peek();
+
+                    match ch {
+                        Some(_) => {
+                            let (_index, ch) = iter.next().unwrap();
+
+                            buffer.push(ch)
                         }
+                        None => todo!(),
                     }
-                    'a'..='z' | 'A'..='Z' | '_' | '.' | '/' | '~' if tokenizer.temp.is_empty() => {
-                        tokenizer.mode = ParseMode::Value;
-                        tokenizer.temp.push(ch);
+                }
+                ' ' => {
+                    tokens.push(generate_token(mode, &buffer));
+                    tokens.push(Token::Space);
+
+                    buffer = String::new();
+                    mode = ParseMode::None;
+                    sub_mode = ParseMode::None;
+                }
+                _ => {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        format!("Invalid character at {}", i),
+                    ))
+                }
+            },
+            ParseMode::SingleQuote => match ch {
+                '\'' => {
+                    tokens.push(generate_token(mode, &buffer));
+
+                    buffer = String::new();
+                    mode = ParseMode::None;
+                    sub_mode = ParseMode::None;
+                }
+                _ => buffer.push(ch),
+            },
+            ParseMode::DoubleQuote => match ch {
+                '"' => {
+                    tokens.push(generate_token(mode, &buffer));
+
+                    buffer = String::new();
+                    mode = ParseMode::None;
+                    sub_mode = ParseMode::None;
+                }
+                '\\' => {
+                    if sub_mode == ParseMode::SingleQuote {
+                        buffer.push(ch);
+                        continue;
                     }
-                    '0'..='9' if tokenizer.temp.is_empty() => {
-                        if let Some((_, '>')) = iter.peek() {
-                            iter.next();
-                            tokenizer.parse_redirector(&mut iter, ch)?;
-                            break;
-                        } else {
-                            tokenizer.temp.push(ch);
-                            tokenizer.mode = ParseMode::Value;
+
+                    match iter.peek() {
+                        Some((_, '\\' | '$' | '"')) => {
+                            let (_index, ch) = iter.next().unwrap();
+
+                            buffer.push(ch)
                         }
+                        Some(_) => {
+                            buffer.push(ch);
+                        }
+                        None => todo!(),
                     }
-                    '>' => {
-                        tokenizer.parse_redirector(&mut iter, '1')?;
-                        break;
+                }
+                '\'' => match sub_mode {
+                    ParseMode::None => {
+                        buffer.push(ch);
+                        sub_mode = ParseMode::SingleQuote;
                     }
-                    ' ' => {
-                        tokenizer.push_space();
+                    ParseMode::SingleQuote => {
+                        buffer.push(ch);
+                        sub_mode = ParseMode::None;
                     }
-                    _ => {
-                        return Err(Error::new(
-                            ErrorKind::InvalidInput,
-                            format!("Invalid character at {}", i),
-                        ))
-                    }
+                    _ => todo!(),
                 },
-                ParseMode::Value => match ch {
-                    'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '.' | '/' => {
-                        tokenizer.temp.push(ch)
-                    }
-                    '\\' => {
-                        let ch = iter.peek();
+                _ => buffer.push(ch),
+            },
+            ParseMode::SingleDashArg | ParseMode::DoubleDashArg => match ch {
+                'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' => buffer.push(ch),
+                ' ' => {
+                    tokens.push(generate_token(mode, &buffer));
+                    tokens.push(Token::Space);
 
-                        match ch {
-                            Some(_) => {
-                                let (_index, ch) = iter.next().unwrap();
-
-                                tokenizer.temp.push(ch)
-                            }
-                            None => todo!(),
-                        }
-                    }
-                    ' ' => {
-                        tokenizer.push_input();
-                        tokenizer.push_space();
-                    }
-                    _ => {
-                        return Err(Error::new(
-                            ErrorKind::InvalidInput,
-                            format!("Invalid character at {}", i),
-                        ))
-                    }
-                },
-                ParseMode::SingleQuote => match ch {
-                    '\'' => tokenizer.push_input(),
-                    _ => tokenizer.temp.push(ch),
-                },
-                ParseMode::DoubleQuote => match ch {
-                    '"' => tokenizer.push_input(),
-                    '\\' => {
-                        if tokenizer.sub_mode == ParseMode::SingleQuote {
-                            tokenizer.temp.push(ch);
-                            continue;
-                        }
-
-                        match iter.peek() {
-                            Some((_, '\\' | '$' | '"')) => {
-                                let (_index, ch) = iter.next().unwrap();
-
-                                tokenizer.temp.push(ch)
-                            }
-                            Some(_) => {
-                                tokenizer.temp.push(ch);
-                            }
-                            None => todo!(),
-                        }
-                    }
-                    '\'' => match tokenizer.sub_mode {
-                        ParseMode::None => {
-                            tokenizer.temp.push(ch);
-                            tokenizer.sub_mode = ParseMode::SingleQuote;
-                        }
-                        ParseMode::SingleQuote => {
-                            tokenizer.temp.push(ch);
-                            tokenizer.sub_mode = ParseMode::None;
-                        }
-                        _ => todo!(),
-                    },
-                    _ => tokenizer.temp.push(ch),
-                },
-                ParseMode::SingleDashArg | ParseMode::DoubleDashArg => match ch {
-                    'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' => tokenizer.temp.push(ch),
-                    ' ' => {
-                        tokenizer.push_input();
-                        tokenizer.push_space();
-                    }
-                    _ => {
-                        return Err(Error::new(
-                            ErrorKind::InvalidInput,
-                            format!("Invalid character at {}", i),
-                        ))
-                    }
-                },
-            }
-        }
-
-        match tokenizer.mode {
-            ParseMode::SingleQuote => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    "Single quote didn't end.",
-                ))
-            }
-            ParseMode::DoubleQuote => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    "Double quote didn't end.",
-                ))
-            }
-            ParseMode::None => {
-                return Ok(tokenizer.tokens.to_vec());
-            }
-            _ => {
-                tokenizer.push_input();
-                return Ok(tokenizer.tokens.to_vec());
-            }
+                    buffer = String::new();
+                    mode = ParseMode::None;
+                    sub_mode = ParseMode::None;
+                }
+                _ => {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        format!("Invalid character at {}", i),
+                    ))
+                }
+            },
         }
     }
 
-    fn parse_redirector(
-        &mut self,
-        iter: &mut Peekable<Enumerate<Chars<'_>>>,
-        prefix: char,
-    ) -> Result<(), Error> {
-        match iter.peek() {
-            Some((_, '>')) => {
-                iter.next();
-
-                self.tokens.push(Token::Appender(prefix));
-                Ok(())
-            }
-            Some(_) => {
-                self.tokens.push(Token::Redirector(prefix));
-                Ok(())
-            }
-
-            None => return Err(Error::new(ErrorKind::InvalidInput, "No redirection target")),
+    match mode {
+        ParseMode::SingleQuote => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Single quote didn't end.",
+            ))
+        }
+        ParseMode::DoubleQuote => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Double quote didn't end.",
+            ))
+        }
+        ParseMode::None => {
+            return Ok(tokens.to_vec());
+        }
+        _ => {
+            tokens.push(generate_token(mode, &buffer));
+            return Ok(tokens.to_vec());
         }
     }
+}
 
-    fn push_input(&mut self) {
-        match self.mode {
-            ParseMode::None => panic!("This shouldn't have happened!"),
-            ParseMode::Value => self.tokens.push(Token::Value(self.temp.to_string())),
-            ParseMode::SingleQuote => self
-                .tokens
-                .push(Token::String(self.temp.to_string(), false)),
-            ParseMode::DoubleQuote => self.tokens.push(Token::String(self.temp.to_string(), true)),
-            ParseMode::SingleDashArg => self
-                .tokens
-                .push(Token::Argument(self.temp.to_string(), false)),
-            ParseMode::DoubleDashArg => self
-                .tokens
-                .push(Token::Argument(self.temp.to_string(), true)),
+fn parse_redirector(
+    iter: &mut Peekable<Enumerate<Chars<'_>>>,
+    prefix: char,
+) -> Result<Token, Error> {
+    match iter.peek() {
+        Some((_, '>')) => {
+            iter.next();
+            Ok(Token::Appender(prefix))
         }
-        self.temp = String::new();
-        self.mode = ParseMode::None;
-        self.sub_mode = ParseMode::None;
+        Some(_) => Ok(Token::Redirector(prefix)),
+        None => return Err(Error::new(ErrorKind::InvalidInput, "No redirection target")),
     }
+}
 
-    fn push_space(&mut self) {
-        if self.tokens.last() != Some(&Token::Space) {
-            self.tokens.push(Token::Space);
-        }
+fn generate_token(mode: ParseMode, value: &str) -> Token {
+    match mode {
+        ParseMode::None => panic!("Tried to push a token before it started to parse anything"),
+        ParseMode::Value => Token::Value(value.to_string()),
+        ParseMode::SingleQuote => Token::String(value.to_string(), false),
+        ParseMode::DoubleQuote => Token::String(value.to_string(), true),
+        ParseMode::SingleDashArg => Token::Argument(value.to_string(), false),
+        ParseMode::DoubleDashArg => Token::Argument(value.to_string(), true),
     }
 }
--- tests/tokenizer.rs
+++ tests/tokenizer.rs
@@ -1,12 +1,15 @@
 use std::io::ErrorKind;
 
-use shell_starter_rust::tokenizer::{Token, Tokenizer};
+use shell_starter_rust::tokenizer::{
+    tokenize,
+    Token::{self, Appender, Argument, Redirector, Space, String, Value},
+};
 
 // Basic
 
 #[test]
 fn empty_input() {
-    let input = "".to_string();
+    let input = "";
     let expected: Vec<Token> = vec![];
 
     assert_parsing(input, expected);
@@ -14,11 +17,11 @@ fn empty_input() {
 
 #[test]
 fn basic_command() {
-    let input = "hello world".to_string();
+    let input = "hello world";
     let expected = vec![
-        Token::Value("hello".to_string()),
-        Token::Space,
-        Token::Value("world".to_string()),
+        Value("hello".to_string()),
+        Space,
+        Value("world".to_string()),
     ];
 
     assert_parsing(input, expected);
@@ -26,11 +29,11 @@ fn basic_command() {
 
 #[test]
 fn multiple_spaces() {
-    let input = "hello                 world".to_string();
+    let input = "hello                 world";
     let expected = vec![
-        Token::Value("hello".to_string()),
-        Token::Space,
-        Token::Value("world".to_string()),
+        Value("hello".to_string()),
+        Space,
+        Value("world".to_string()),
     ];
 
     assert_parsing(input, expected);
@@ -40,11 +43,11 @@ fn multiple_spaces() {
 
 #[test]
 fn single_quote() {
-    let input = "echo 'example test'".to_string();
+    let input = "echo 'example test'";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("example test".to_string(), false),
+        Value("echo".to_string()),
+        Space,
+        String("example test".to_string(), false),
     ];
 
     assert_parsing(input, expected);
@@ -52,16 +55,16 @@ fn single_quote() {
 
 #[test]
 fn unclosed_single_quote() {
-    assert_parsing_err(String::from("echo 'unclosed single quote"));
+    assert_parsing_err("echo 'unclosed single quote");
 }
 
 #[test]
 fn double_quote() {
-    let input = "echo \"hello world\"".to_string();
+    let input = "echo \"hello world\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello world".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello world".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -69,18 +72,18 @@ fn double_quote() {
 
 #[test]
 fn unclosed_double_quote() {
-    let input = "echo \"unclosed double quote".to_string();
+    let input = "echo \"unclosed double quote";
 
     assert_parsing_err(input);
 }
 
 #[test]
 fn double_quotes_with_escaped_characters() {
-    let input = "echo \"escaped \\\"double quotes\\\"\"".to_string();
+    let input = "echo \"escaped \\\"double quotes\\\"\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("escaped \"double quotes\"".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("escaped \"double quotes\"".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -88,11 +91,11 @@ fn double_quotes_with_escaped_characters() {
 
 #[test]
 fn double_quotes_with_wide_space() {
-    let input = "echo \"hello                   world\"".to_string();
+    let input = "echo \"hello                   world\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello                   world".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello                   world".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -100,11 +103,11 @@ fn double_quotes_with_wide_space() {
 
 #[test]
 fn escaped_backslash_in_double_quote() {
-    let input = "echo \"hello\\\\world\"".to_string();
+    let input = "echo \"hello\\\\world\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello\\world".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello\\world".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -112,11 +115,11 @@ fn escaped_backslash_in_double_quote() {
 
 #[test]
 fn double_inside_single_quote() {
-    let input = "echo '\"hello world\"'".to_string();
+    let input = "echo '\"hello world\"'";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("\"hello world\"".to_string(), false),
+        Value("echo".to_string()),
+        Space,
+        String("\"hello world\"".to_string(), false),
     ];
 
     assert_parsing(input, expected);
@@ -124,11 +127,11 @@ fn double_inside_single_quote() {
 
 #[test]
 fn single_inside_double_quote() {
-    let input = "echo \"'hello world'\"".to_string();
+    let input = "echo \"'hello world'\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("'hello world'".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("'hello world'".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -136,17 +139,17 @@ fn single_inside_double_quote() {
 
 #[test]
 fn mixed_quotes_and_arguments() {
-    let input = "cmd 'single' \"double\" --arg1 -a".to_string();
+    let input = "cmd 'single' \"double\" --arg1 -a";
     let expected = vec![
-        Token::Value("cmd".to_string()),
-        Token::Space,
-        Token::String("single".to_string(), false),
-        Token::Space,
-        Token::String("double".to_string(), true),
-        Token::Space,
-        Token::Argument("arg1".to_string(), true),
-        Token::Space,
-        Token::Argument("a".to_string(), false),
+        Value("cmd".to_string()),
+        Space,
+        String("single".to_string(), false),
+        Space,
+        String("double".to_string(), true),
+        Space,
+        Argument("arg1".to_string(), true),
+        Space,
+        Argument("a".to_string(), false),
     ];
 
     assert_parsing(input, expected);
@@ -156,13 +159,13 @@ fn mixed_quotes_and_arguments() {
 
 #[test]
 fn single_dash_argument() {
-    let input = "echo -s 'hello world'".to_string();
+    let input = "echo -s 'hello world'";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::Argument("s".to_string(), false),
-        Token::Space,
-        Token::String("hello world".to_string(), false),
+        Value("echo".to_string()),
+        Space,
+        Argument("s".to_string(), false),
+        Space,
+        String("hello world".to_string(), false),
     ];
 
     assert_parsing(input, expected);
@@ -170,13 +173,13 @@ fn single_dash_argument() {
 
 #[test]
 fn double_dash_argument() {
-    let input = "echo --silent 'hello world'".to_string();
+    let input = "echo --silent 'hello world'";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::Argument("silent".to_string(), true),
-        Token::Space,
-        Token::String("hello world".to_string(), false),
+        Value("echo".to_string()),
+        Space,
+        Argument("silent".to_string(), true),
+        Space,
+        String("hello world".to_string(), false),
     ];
 
     assert_parsing(input, expected);
@@ -184,15 +187,15 @@ fn double_dash_argument() {
 
 #[test]
 fn redirection_operator() {
-    let input = "echo \"hello world\" > \"./hello.md\"".to_string();
+    let input = "echo \"hello world\" > \"./hello.md\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello world".to_string(), true),
-        Token::Space,
-        Token::Redirector('1'),
-        Token::Space,
-        Token::String("./hello.md".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello world".to_string(), true),
+        Space,
+        Redirector('1'),
+        Space,
+        String("./hello.md".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -200,15 +203,15 @@ fn redirection_operator() {
 
 #[test]
 fn error_redirection_operator() {
-    let input = "echo \"hello world\" 2> \"./hello.md\"".to_string();
+    let input = "echo \"hello world\" 2> \"./hello.md\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello world".to_string(), true),
-        Token::Space,
-        Token::Redirector('2'),
-        Token::Space,
-        Token::String("./hello.md".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello world".to_string(), true),
+        Space,
+        Redirector('2'),
+        Space,
+        String("./hello.md".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -216,20 +219,20 @@ fn error_redirection_operator() {
 
 #[test]
 fn redirection_without_target() {
-    assert_parsing_err(String::from("echo >"));
+    assert_parsing_err("echo >");
 }
 
 #[test]
 fn appender() {
-    let input = "echo \"hello world\" >> \"./hello.md\"".to_string();
+    let input = "echo \"hello world\" >> \"./hello.md\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello world".to_string(), true),
-        Token::Space,
-        Token::Appender('1'),
-        Token::Space,
-        Token::String("./hello.md".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello world".to_string(), true),
+        Space,
+        Appender('1'),
+        Space,
+        String("./hello.md".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -237,15 +240,15 @@ fn appender() {
 
 #[test]
 fn appender_with_number() {
-    let input = "echo \"hello world\" 2>> \"./hello.md\"".to_string();
+    let input = "echo \"hello world\" 2>> \"./hello.md\"";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("hello world".to_string(), true),
-        Token::Space,
-        Token::Appender('2'),
-        Token::Space,
-        Token::String("./hello.md".to_string(), true),
+        Value("echo".to_string()),
+        Space,
+        String("hello world".to_string(), true),
+        Space,
+        Appender('2'),
+        Space,
+        String("./hello.md".to_string(), true),
     ];
 
     assert_parsing(input, expected);
@@ -253,20 +256,20 @@ fn appender_with_number() {
 
 #[test]
 fn invalid_character() {
-    let input = "echo hello @world".to_string();
+    let input = "echo hello @world";
 
     assert_parsing_err(input);
 }
 
 #[test]
 fn mixed_quotes() {
-    let input = "echo \"double quotes\" 'single quotes'".to_string();
+    let input = "echo \"double quotes\" 'single quotes'";
     let expected = vec![
-        Token::Value("echo".to_string()),
-        Token::Space,
-        Token::String("double quotes".to_string(), true),
-        Token::Space,
-        Token::String("single quotes".to_string(), false),
+        Value("echo".to_string()),
+        Space,
+        String("double quotes".to_string(), true),
+        Space,
+        String("single quotes".to_string(), false),
     ];
 
     assert_parsing(input, expected);
@@ -281,15 +284,15 @@ fn assert_vec_eq<T: std::fmt::Debug + PartialEq>(vec1: &[T], vec2: &[T]) {
     }
 }
 
-fn assert_parsing(input: String, expected: Vec<Token>) {
-    match Tokenizer::tokenize(input) {
+fn assert_parsing(input: &str, expected: Vec<Token>) {
+    match tokenize(input) {
         Ok(tokens) => assert_vec_eq(&tokens, &expected),
         Err(err) => panic!("Unexpected error: {}", err),
     }
 }
 
-fn assert_parsing_err(input: String) {
-    let result = Tokenizer::tokenize(input);
+fn assert_parsing_err(input: &str) {
+    let result = tokenize(input);
 
     assert!(result.is_err());
     assert_eq!(result.err().unwrap().kind(), ErrorKind::InvalidInput);
