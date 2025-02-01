b1c26cf84c0940e4f613a3cb6448147e3ef180d7
Emre Can Çorapçı <emrecancorapci@gmail.com>
2025-02-01 21:59:07 +0300

    Update shell to enable raw mode and implement basic movements

--- src/shell.rs
+++ src/shell.rs
@@ -2,9 +2,14 @@ use std::io::{self, Error, ErrorKind, Stderr, Stdout, Write};
 
 use core::{ShellCommandProvider, ShellInterpreter, ShellTokenizer};
 use crossterm::{
-    event::{self, Event, KeyCode, KeyEvent},
+    cursor::{self, MoveRight, MoveToColumn},
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     execute,
+    style::Print,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
+
+const PREFIX: &str = "$ ";
 
 pub mod core;
 
@@ -12,6 +17,7 @@ pub struct Shell {
     buffer: String,
     stdout: Stdout,
     stderr: Stderr,
+    // history: Vec<String>,
 }
 
 impl Shell {
@@ -20,43 +26,78 @@ impl Shell {
             buffer: String::new(),
             stdout: io::stdout(),
             stderr: io::stderr(),
+            // history: Vec::new(),
         }
     }
 
-    pub async fn run<T, SI: ShellInterpreter<T>, ST: ShellTokenizer<T>, SCC: ShellCommandProvider<T>>(
+    pub async fn run<
+        T,
+        SI: ShellInterpreter<T>,
+        ST: ShellTokenizer<T>,
+        SCC: ShellCommandProvider<T>,
+    >(
         &mut self,
-    ) -> Result<String, Error> {
+    ) -> Result<(), Error> {
         self.init()?;
-
-        print!("$ ");
 
         loop {
             self.stdout.flush()?;
 
-            match match event::read()? {
-                Event::FocusGained => todo!(),
-                Event::FocusLost => todo!(),
-                Event::Key(key_event) => self.handle_keys::<T, SI, ST, SCC>(key_event),
-                Event::Mouse(_) => todo!(),
-                Event::Paste(_) => todo!(),
-                Event::Resize(_, _) => todo!(),
-            } {
-                Ok(_) => {}
-                Err(err) => {
-                    if err.kind() == ErrorKind::Interrupted {
-                        return Err(err);
-                    }
-                }
+            let result = self.shell_loop::<T, SI, ST, SCC>();
+
+            if result.is_err() && result.unwrap_err().kind() == ErrorKind::Interrupted {
+                break;
             }
+        }
+
+        self.uninit()?;
+
+        Ok(())
+    }
+
+    fn shell_loop<
+        T,
+        SI: ShellInterpreter<T>,
+        ST: ShellTokenizer<T>,
+        SCC: ShellCommandProvider<T>,
+    >(
+        &mut self,
+    ) -> Result<(), Error> {
+        match event::read()? {
+            Event::FocusGained => todo!(),
+            Event::FocusLost => todo!(),
+            Event::Key(key_event) => {
+                self.handle_modifiers(key_event)?;
+                self.handle_keys::<T, SI, ST, SCC>(key_event)?;
+
+                Ok(())
+            }
+            Event::Mouse(_) => todo!(),
+            Event::Paste(_) => todo!(),
+            Event::Resize(_, _) => todo!(),
         }
     }
 
     fn init(&mut self) -> Result<(), Error> {
+        enable_raw_mode()?;
+
+        execute!(self.stdout, Print("$ "),)?;
+        Ok(())
+    }
+
+    fn uninit(&mut self) -> Result<(), Error> {
+        disable_raw_mode()?;
+
         execute!(self.stdout)?;
         Ok(())
     }
 
-    fn handle_keys<T, SI: ShellInterpreter<T>, ST: ShellTokenizer<T>, SCC: ShellCommandProvider<T>>(
+    fn handle_keys<
+        T,
+        Interpreter: ShellInterpreter<T>,
+        Tokenizer: ShellTokenizer<T>,
+        CommandProvider: ShellCommandProvider<T>,
+    >(
         &mut self,
         key_event: KeyEvent,
     ) -> Result<(), Error> {
@@ -64,39 +105,74 @@ impl Shell {
 
         match code {
             KeyCode::Char(ch) => {
-                self.buffer.push(ch);
-            }
-            KeyCode::Backspace => {}
-            KeyCode::Enter => {
-                match ST::tokenize(self.buffer.trim())? {
-                    tokens if tokens.is_empty() => {
-                        self.stdout.write(b"")?;
-                    }
-                    tokens => match SI::run::<SCC>(&tokens) {
-                        Ok(ok) => {
-                            self.stdout.write(&ok)?;
-                        }
-                        Err(err) => {
-                            if err.kind() == ErrorKind::Interrupted {
-                                return Err(err);
-                            }
-                            self.stderr.write(&err.to_string().as_bytes())?;
-                        }
-                    },
-                }
+                let relative_cursor_x = cursor::position()?.0 as usize - PREFIX.len();
 
-                self.stdout.write(b"\n$ ")?;
+                if relative_cursor_x < self.buffer.len() {
+                    self.buffer.insert(relative_cursor_x, ch);
+
+                    execute!(
+                        self.stdout,
+                        Clear(ClearType::CurrentLine),
+                        MoveToColumn(0),
+                        Print(PREFIX),
+                        Print(&self.buffer),
+                        MoveToColumn((relative_cursor_x + 3) as u16)
+                    )?;
+                } else {
+                    self.buffer.push(ch);
+                    execute!(self.stdout, Print(ch))?;
+                }
+            }
+            KeyCode::Enter => {
+                if !self.buffer.is_empty() {
+                    execute!(self.stdout, Print("\r\n"))?;
+
+                    match Tokenizer::tokenize(self.buffer.trim())? {
+                        tokens => match Interpreter::run::<CommandProvider>(&tokens) {
+                            Ok(ok) => {
+                                self.stdout.write(&ok)?;
+                            }
+                            Err(err) => {
+                                if err.kind() == ErrorKind::Interrupted {
+                                    return Err(err);
+                                }
+                                self.stderr.write(&err.to_string().as_bytes())?;
+                            }
+                        },
+                    }
+                }
+                execute!(self.stdout, Print("\r\n"), Print(PREFIX))?;
+
                 self.buffer.clear();
             }
-            KeyCode::Left => {}
-            KeyCode::Right => {}
+            KeyCode::Tab => {}
+            KeyCode::Backspace => {
+                self.buffer.pop();
+
+                execute!(
+                    self.stdout,
+                    Clear(ClearType::CurrentLine),
+                    MoveToColumn(0),
+                    Print(PREFIX),
+                    Print(&self.buffer)
+                )?;
+            }
+            KeyCode::Left => {
+                execute!(self.stdout, cursor::MoveLeft(1))?;
+            }
+            KeyCode::Right => {
+                let relative_cursor_x = cursor::position()?.0 as usize - PREFIX.len();
+
+                if relative_cursor_x < self.buffer.len() {
+                    execute!(self.stdout, MoveRight(1))?;
+                }
+            }
             KeyCode::Up => todo!(),
             KeyCode::Down => todo!(),
             KeyCode::Home => todo!(),
             KeyCode::End => todo!(),
             KeyCode::PageUp => todo!(),
             KeyCode::PageDown => todo!(),
-            KeyCode::Tab => todo!(),
             KeyCode::BackTab => todo!(),
             KeyCode::Delete => todo!(),
             KeyCode::Insert => todo!(),
@@ -112,6 +188,18 @@ impl Shell {
             KeyCode::KeypadBegin => todo!(),
             KeyCode::Media(_) => todo!(),
             KeyCode::Modifier(_) => todo!(),
+        }
+
+        Ok(())
+    }
+
+    fn handle_modifiers(&self, key_event: KeyEvent) -> Result<(), Error> {
+        let KeyEvent {
+            code, modifiers, ..
+        } = key_event;
+
+        if modifiers.contains(KeyModifiers::CONTROL) && matches!(code, KeyCode::Char('c')) {
+            return Err(io::Error::new(ErrorKind::Interrupted, "ctrl-c"));
         }
 
         Ok(())
